55c54e72fe8794828041c26ddb48e003d9488a2c
Daviesss <oguns53@gmail.com>
2026-06-09 11:52:54 +0000

    Initial Commit

--- ridescan_ros2_bridge/setup.py
+++ ridescan_ros2_bridge/setup.py
@@ -24,7 +24,14 @@ setup(
     },
     entry_points={
         'console_scripts': [
+        'ride_scan_ros2_bridge= ridescan_ros2_bridge.ridescan_bridge_node:main',
+        'ride_scan_test_ros2_bridge= ridescan_ros2_bridge.ridescan_test_bridge_node:main',
+        'ride_scan_diagnostics= ridescan_ros2_bridge.ridescan_diagnostics:main',
+        'ride_scan_safety_monitor= ridescan_ros2_bridge.ridescan_safety_node:main',
+        'ride_scan_risk_plot= ridescan_ros2_bridge.ridescan_risk_plot:main',
         'ride_scan_csv_node= ridescan_ros2_bridge.ridescan_bride_csv_node:main',
+        'ride_scan_csv_diagnostics= ridescan_ros2_bridge.ridescan_csv_diagnostics:main',
+        'way_point_follower_node= ridescan_ros2_bridge.way_point_follower:main', # way point follower node script , robot navigates to the same waypoint in a map or environment repeatedly, often for testing or demonstration purposes.
         ],
     },
 )
